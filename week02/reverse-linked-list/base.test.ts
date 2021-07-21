2aeb85502999c7d22fcf3009197f38c6f374ad44
王建明 <wangjianming.x@gmail.com>
2021-07-21 23:32:09 +0800

    week02/链表反转

--- week02/reverse-linked-list/base.test.ts
+++ week02/reverse-linked-list/base.test.ts
@@ -1,13 +1,6 @@
 import reverseLinkedListWithRecursion from "./reverse-linked-list-with-recursion";
+import reverseLinkedListWithLoop from "./reverse-linked-list-with-loop";
 import LinkedListNode from "./LinkedListNode";
-
-const linkedList = new LinkedListNode(
-  1,
-  new LinkedListNode(
-    2,
-    new LinkedListNode(3, new LinkedListNode(4, new LinkedListNode(5)))
-  )
-);
 
 function toArray(ll: LinkedListNode | null) {
   const arr = [];
@@ -22,11 +15,37 @@ function toArray(ll: LinkedListNode | null) {
 }
 
 test("reverse-linked-list-with-recursion", () => {
+  const linkedList = new LinkedListNode(
+    1,
+    new LinkedListNode(
+      2,
+      new LinkedListNode(3, new LinkedListNode(4, new LinkedListNode(5)))
+    )
+  );
+
   expect(toArray(linkedList)).toEqual([1, 2, 3, 4, 5]);
 
   expect(toArray(reverseLinkedListWithRecursion(null))).toEqual([]);
 
   expect(toArray(reverseLinkedListWithRecursion(linkedList))).toEqual([
+    5, 4, 3, 2, 1,
+  ]);
+});
+
+test("reverse-linked-list-with-loop", () => {
+  const linkedList = new LinkedListNode(
+    1,
+    new LinkedListNode(
+      2,
+      new LinkedListNode(3, new LinkedListNode(4, new LinkedListNode(5)))
+    )
+  );
+
+  expect(toArray(linkedList)).toEqual([1, 2, 3, 4, 5]);
+
+  expect(toArray(reverseLinkedListWithLoop(null))).toEqual([]);
+
+  expect(toArray(reverseLinkedListWithLoop(linkedList))).toEqual([
     5, 4, 3, 2, 1,
   ]);
 });
